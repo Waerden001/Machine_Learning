71632d3420af519e6755e0f313e64f213bc5525d
Waerden <42280958+Waerden001@users.noreply.github.com>
2019-06-16 22:04:36 -0400

    add Move class

--- AlphaGoZero/goboard_slow.py
+++ AlphaGoZero/goboard_slow.py
@@ -1,1 +1,21 @@
-
+class Move():
+  
+  def __init__(self, point=None, is_pass=False, is_resign=False):
+    assert (point is not None) or is_pass or is_resign
+    self.point = point
+    self.is_play = (self.point is not None)
+    self.is_pass = is_pass
+    self.is_resign = is_resign
+  
+  
+  @classmethod
+  def play(cls, point):
+    return Move(point=point)
+  
+  @classmethod
+  def pass_turn(cls):
+    return Move(is_pass=True)
+  
+  @classmethod
+  def resign(cls):
+    return Move(is_resign=True)
