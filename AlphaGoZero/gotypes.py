887d99f2f6a3f4c210adc8cafaa239f70b78ce65
Waerden <42280958+Waerden001@users.noreply.github.com>
2019-06-16 21:52:14 -0400

    add Point class, inherited from namedtuple

--- AlphaGoZero/gotypes.py
+++ AlphaGoZero/gotypes.py
@@ -10,3 +10,15 @@ class Player(enum.Enum):
   def other(self):
     return Player.black if self == Player.white else Player.black
   
+
+from collections import namedtuple
+
+
+class Point(namedtuple('Point', 'row, col')):
+  
+  def neighbors(self):
+    
+    return [Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1)]
